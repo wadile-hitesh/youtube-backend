21358ca6d64d6527beca4f880511c2f729dde597
wadile-hitesh <hiteshwadile.dev@gmail.com>
2024-02-09 19:59:24 +0530

    Video and User Model Created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //require('dotenv').config({path : './env})
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 const port = process.env.PORT || 8000
 
 dotenv.config({

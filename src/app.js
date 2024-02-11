bf5691c3ae5ba24e261226dc4b6019d5b7cb1b3b
wadile-hitesh <hiteshwadile.dev@gmail.com>
2024-02-11 14:13:42 +0530

    All Setup Configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.urlencoded({extended : true, limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// Routes
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users",userRouter)
+
 export {app}
